bdb4efc947d4e652d119fd6833164e38c4588897
Joshua <jbautista396@gmail.com>
2020-05-12 19:20:03 -0400

    Fix discord widget

--- src/views/UserList/UserList.js
+++ src/views/UserList/UserList.js
@@ -12,7 +12,7 @@ const UserList = () => {
         </Typography>
       </Grid>
       <Grid item style={{margin: "auto", marginTop: "10vh"}}>
-        <iframe title="discord-widget" src="https://discordapp.com/widget?id=167039507663814657&theme=dark" style={{width: "350px", height: "500px"}} allowTransparency={true} frameBorder={0} />
+        <iframe title="discord-widget" src="https://discordapp.com/widget?id=167039507663814657&theme=dark" style={{width: "350px", height: "500px"}} frameBorder={0} />
       </Grid>
     </Grid>
   );
